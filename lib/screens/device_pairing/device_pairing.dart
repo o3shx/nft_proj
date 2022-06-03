0b7a25e2ae3b43f478804c698a4f298d4dc32cdd
Oshada Palitharathna <52082057+o3shx@users.noreply.github.com>
2022-06-03 13:26:04 +0400

    App Logic for Scanning and Connecting with BLE device.
    Data transmission is to be tested as the _sendData method does not trigger any response from device.

--- lib/screens/device_pairing/device_pairing.dart
+++ lib/screens/device_pairing/device_pairing.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 class DevicePairingPage extends StatefulWidget {
   const DevicePairingPage({Key? key}) : super(key: key);
@@ -8,8 +11,123 @@ class DevicePairingPage extends StatefulWidget {
 }
 
 class _DevicePairingPageState extends State<DevicePairingPage> {
+  bool _foundDeviceWaitingToConnect = false;
+  bool _scanStarted = false;
+  bool _connected = false;
+  late DiscoveredDevice _ubiqueDevice;
+  final flutterReactiveBle = FlutterReactiveBle();
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late QualifiedCharacteristic _rxCharacteristic;
+  final Uuid serviceUuid = Uuid.parse("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+  final Uuid characteristicUuid =
+      Uuid.parse("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+
+  void _startScan() async {
+    setState(() {
+      _scanStarted = true;
+    });
+
+    if (true) {
+      _scanStream = flutterReactiveBle
+          .scanForDevices(withServices: [serviceUuid]).listen((device) {
+        setState(() {
+          _ubiqueDevice = device;
+          _foundDeviceWaitingToConnect = true;
+        });
+      });
+    }
+  }
+
+  void _connectToDevice() {
+    _scanStream.cancel();
+    Stream<ConnectionStateUpdate> _currentConnectionStream = flutterReactiveBle
+        .connectToAdvertisingDevice(
+            id: _ubiqueDevice.id,
+            prescanDuration: const Duration(seconds: 1),
+            withServices: [serviceUuid, characteristicUuid]);
+    _currentConnectionStream.listen((event) {
+      switch (event.connectionState) {
+        case DeviceConnectionState.connected:
+          {
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: serviceUuid,
+                characteristicId: characteristicUuid,
+                deviceId: event.deviceId);
+            setState(() {
+              _foundDeviceWaitingToConnect = false;
+              _connected = true;
+            });
+            break;
+          }
+        case DeviceConnectionState.disconnected:
+          {
+            break;
+          }
+        default:
+      }
+    });
+  }
+
+  void _sendData() {
+    if (_connected) {
+      flutterReactiveBle
+          .writeCharacteristicWithResponse(_rxCharacteristic, value: [
+            0xff,
+          ])
+          .whenComplete(() => debugPrint("Complete"))
+          .onError((error, stackTrace) => debugPrint(error.toString()));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (_connected) {
+      return Center(
+        child: Column(
+          children: [
+            const Text("Device Connected"),
+            ElevatedButton(
+              child: const Text("Send Data"),
+              onPressed: _sendData,
+            ),
+          ],
+        ),
+      );
+    } else {
+      if (_foundDeviceWaitingToConnect) {
+        return Center(
+          child: Column(
+            children: [
+              const Text("Device Found"),
+              Text(_ubiqueDevice.name.toString()),
+              Text(_ubiqueDevice.id.toString()),
+              Text(_ubiqueDevice.manufacturerData.toString()),
+              ElevatedButton(
+                child: const Text("Connect to Device"),
+                onPressed: _connectToDevice,
+              ),
+            ],
+          ),
+        );
+      } else {
+        if (_scanStarted) {
+          return const Center(
+            child: Text("Scannig for device"),
+          );
+        } else {
+          return Center(
+            child: ElevatedButton(
+              child: const Text("Start Scan"),
+              onPressed: _startScan,
+            ),
+          );
+        }
+      }
+    }
   }
 }
